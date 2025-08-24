68cf0ba2c0e381b75bcc79a09369ca7de1115692
SerKo <serko.dev@gmail.com>
2025-08-24 22:22:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# markdown-fit
+
+A TypeScript rewrite of markdown-it with enhancements
+
+## Development
+
+- Install dependencies:
+
+```bash
+pnpm install
+```
+
+- Run the unit tests:
+
+```bash
+pnpm run test
+```
+
+- Build the library:
+
+```bash
+pnpm run build
+```
---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    pnpm: true,
+  },
+  {
+    rules: {
+      'no-console': 'warn',
+      'no-alert': 'warn',
+      'curly': 'off',
+      'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+      'unused-imports/no-unused-vars': 'warn',
+      'style/operator-linebreak': 'off',
+    },
+  },
+)
