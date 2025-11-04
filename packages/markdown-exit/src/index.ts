ce6828a11bce5e3b93f6fadef6292ab242e23024
SerKo <serko.dev@gmail.com>
2025-11-04 19:19:17 +0800

    refactor: move hybrid types callable construct to index

---
+++ packages/markdown-exit/src/index.ts
@@ -0,0 +1,33 @@
+import type { createMarkdownExit } from './core'
+import { MarkdownExit } from './core'
+
+export * from './core'
+
+// hybrid types callable construct signatures hack
+
+/**
+ * Make class callable without `new` operator.
+ */
+function createCallableClass<T extends new (...args: any) => any>(Class: T) {
+  function callable(...args: ConstructorParameters<T>): InstanceType<T> {
+    return new Class(...args)
+  }
+
+  // bridge statics
+  Object.setPrototypeOf(callable, MarkdownExit)
+
+  // share the same instance prototype
+  ;(callable as any).prototype = MarkdownExit.prototype
+  ;(callable as any).prototype.constructor = callable
+
+  return callable as unknown as T
+}
+
+// type and default const variable name must be the same
+// for correct d.ts generation
+type MarkdownExitConstructor = InstanceType<typeof MarkdownExit>
+
+// eslint-disable-next-line ts/no-redeclare
+const MarkdownExitConstructor = createCallableClass(MarkdownExit) as unknown as (typeof createMarkdownExit & typeof MarkdownExit)
+
+export default MarkdownExitConstructor
