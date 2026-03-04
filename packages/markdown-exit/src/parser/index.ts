ce4dfb3f5874c43e58ccf86a2a093081f140f43c
SerKo <serko.dev@gmail.com>
2026-03-04 15:32:06 +0800

    feat: export state types (resolve #26)

--- packages/markdown-exit/src/parser/index.ts
+++ packages/markdown-exit/src/parser/index.ts
@@ -1,5 +1,11 @@
 export type { RuleBlock } from './block/parser_block'
+export { default as StateBlock } from './block/state_block'
+
 export type { RuleCore } from './core/parser_core'
+export { default as StateCore } from './core/state_core'
+
 export type { RuleInline } from './inline/parser_inline'
+export { default as StateInline } from './inline/state_inline'
+
 export * from './parser'
 export * from './ruler'
